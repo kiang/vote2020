b6d97efcdafd3afef7655d90a381fa18ff931a45
auto commit <noreply@localhost>
2019-12-24 14:34:07 +0800

    auto update @ 2019-12-24 14:33:39

---
+++ cron.php
@@ -0,0 +1,9 @@
+<?php
+$rootPath = __DIR__;
+$now = date('Y-m-d H:i:s');
+
+exec("cd {$rootPath} && /usr/bin/git pull");
+exec("/usr/bin/php -q {$rootPath}/01_fetch.php");
+exec("cd {$rootPath} && /usr/bin/git add -A");
+exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+exec("cd {$rootPath} && /usr/bin/git push origin master");
